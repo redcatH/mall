aa6b01d23ecc471d74f671f73972f13e48bc99e3
刘毅 <ihaolin@outlook.com>
2020-04-10 12:31:05 +0800

    CORS跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,22 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  data(){
+    return {
+      age:30
+    }
+  },
+  mounted(){
+    let url="https://www.easy-mock.com/mock/5e8fee48572a914be2e35422/example/query?name=5";
+    axios.get(url).then(()=>{
+
+    })
+   }
 }
 </script>
